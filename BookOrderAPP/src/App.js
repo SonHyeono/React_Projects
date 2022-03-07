0fe0cb5beece51210ff916ca33738e661ae22bab
SonHyeonoSonny <oktlrp7624583@gmail.com>
2022-03-07 17:54:19 +0900

    React Project 2
    React 2번째 프로젝트는 책 주문 앱이고 아직 개발중.

---
+++ BookOrderAPP/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Layout/Header'
+import Main from './components/Layout/Main'
+import Books from './components/Books/Books'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main>
+        <Books />
+      </Main>
+    
+    </>
+  )
+}
+
+export default App
